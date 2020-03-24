e415420090aeb80528ef76afb9e3dc56f6dddd10
unknown <aminmirzoev123@gmail.com>
2020-03-24 18:27:13 +0500

    HW#1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Amin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+           /* double a=16.80, b=12.40;
+            double c=Math.Sqrt(a*b);
+            Console.WriteLine(c);*/
+
+            //task 2
+           /* double A=1.4, B=-5.5, C=0.6;
+            double AC=A-C;
+            double BC=(B*(-1)+C);
+            double amount =AC+BC;
+            Console.WriteLine($"result AC={AC} result BC={BC} result={amount}");*/
+
+            //task 3
+            /* double X1=-6.2,Y1=5.2;
+            double X2=2.1,Y2=9.8;
+            double s=Math.Sqrt(Math.Pow(X1-X2,2)+Math.Pow(Y1-Y2,2));
+            Console.WriteLine($"result={s}");*/
+
+            //task 4
+            /* int a=41;
+            int b=a/10;
+            int c=a%10;
+            Console.Write($"result={c}{b}");*/
+
+            //task 5
+           /*  int N=10985;
+            int S=N/60;
+            Console.WriteLine(S);*/
+
+            //task 6
+            // int d = 202;
+            // System.Console.WriteLine(d%7);
+
+
+
+
+            
+        }
+    }
+}
